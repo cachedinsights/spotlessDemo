d79c38ce4fa2fa8be4a95ef5b7a55510ffba46b4
stackfortech <mdeo2205@gmail.com>
2020-09-25 23:38:07 +0530

    Initial commit

---
+++ src/main/java/com/stackfortech/spotlessDemo/SpotlessDemoApplication.java
@@ -0,0 +1,13 @@
+package com.stackfortech.spotlessDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlessDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlessDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stackfortech/spotlessDemo/SpotlessDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackfortech.spotlessDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
